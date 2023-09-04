2b0fe46782a29544114eff5b798ba707258ad2d6
Sofya <vanieva.sofya@mail.ru>
2023-09-04 13:30:33 +0300

    064Ready

--- Task064/Program.cs
+++ Task064/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте значение N. 
+Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"8*/
+
+
+
+using System;
+
+Console.Write("Введите натуральное число N: ");
+int n = Console.Read();
+PrintNumbers(n);
+
+void PrintNumbers(int n)
+{
+    if (n == 1)
+    {
+        Console.Write(n);
+    }
+    else
+    {
+        Console.Write(n + ", ");
+        PrintNumbers(n - 1);
+    }
+}
